aee9373b660acff2b98468845a1f8cd1f8463706
Alberto Contento <alberto.contento@factorymind.com>
2023-04-19 16:46:25 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var http = require('http');
+const fs = require('fs');
+let rawdata = fs.readFileSync('config.json');
+
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.setHeader('Content-Type', 'application/json');
-    res.write(config.json);
+    res.write(JSON.parse(rawdata));
     res.end();
 }).listen(process.env.PORT || 3000);
