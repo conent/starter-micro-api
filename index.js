e87c6f29cd2f77ec110750d54fd8af12264f15f8
Alberto Contento <alberto.contento@factorymind.com>
2023-04-21 13:20:25 +0200

    test fauna

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 // Read the config file
 let rawdata = fs.readFileSync('./config.json');
 
+var faunaDb = require('faunadb')
+var fQuery = faunaDb.query
+
 http
   .createServer(function (req, res) {
     var url = req.url;
@@ -18,6 +21,17 @@ http
     else if(url ==='/config'){
       res.setHeader('Content-Type', 'application/json');
       res.end(rawdata);
+    } else if(url ==='testFauna'){
+      var client = new faunaDb.Client({ 
+        secret: 'fnAFCDv6dmAAzPS1JG9-TrDSx3Do3M5Q0gOMhScV',
+        endpoint: 'https://dashboard.fauna.com/'
+      })
+      q.Create(
+        q.Collection('ChiudiLaPorta'),
+        { data: { testField: 'testValue' } }
+      )
+      .then(function (res) { console.log('Result:', res) })
+      .catch(function (err) { console.log('Error:', err) })
     } else{
       res.setHeader('Content-Type', 'text/html');
       res.write('<h1>Hello World!<h1>'); //write a response
